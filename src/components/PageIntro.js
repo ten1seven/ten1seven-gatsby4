8b5f79f42e06d7737e14ecc614fe05269465c08e
Jeremy Fields <jeremy.fields@viget.com>
2022-02-17 16:43:19 -0700

    Update global font size

--- src/components/PageIntro.js
+++ src/components/PageIntro.js
@@ -6,13 +6,17 @@ import nl2br from "react-nl2br"
 class PageIntro extends React.Component {
   render() {
     return (
-      <p className="
+      <p
+        className="
         border-b
         border-gray-light
         italic
         my-8
         pb-8
-        text-xl">{nl2br(this.props.intro)}</p>
+        text-xl"
+      >
+        {nl2br(this.props.intro)}
+      </p>
     )
   }
 }
